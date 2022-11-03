316b7f7a86085c69e9f5a30f686aad787354fd09
Kristy L <kristyle96@yahoo.com>
2022-11-03 08:07:04 -0700

    Created a list of 88rising artists and Spotify id

---
+++ artists_88rising.py
@@ -0,0 +1,8 @@
+artists = ["Atarashii Gakko!", "Bibi", "Chungha","Dumbfoundead","Guapdad 4000", "Higher Brothers","Jackson Wang","Joji",
+"Keith Ape",
+"Lexie Liu",
+"Milli",
+"Niki", "Rich Brian","Seori",
+"Stephanie Poetri",
+"Warren Hue",
+"Ylona Garcia"]
