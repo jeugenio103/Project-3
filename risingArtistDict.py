bbf82b7f992e1d48e1cdc0f2dbcffd38de0eae13
Master-Leo <emillio3198@gmail.com>
2022-11-03 11:57:48 -0700

    'created 88 dict'

--- risingArtistDict.py
+++ risingArtistDict.py
@@ -1,8 +1,3 @@
-risingArtists = ['Joji': 30050750, 'NIKI': 10007653, 'Rich Brian': 8996136,'BIBI': 3588317,'Jackson Wang': 2677250
-'CHUNG HA':
-2667708,
-'Stephanie Poetri':
-1620564,
-'Higher Brothers':
-1577571]
+artists = {'name': 'Joji', 'followers': 30050750, 'name': 'NIKI', 'followers': 10007653, 'name': 'Rich Brian', 'followers': 8996136,'name': 'BIBI', 'followers': 3588317, 'name': 'Jackson Wang', 'followers': 2677250,
+'name': 'CHUNG HA', 'followers': 2667708, 'name': 'Stephanie Poetri', 'followers': 1620564, 'name': 'Higher Brothers', 'followers': 1577571, 'name': 'Milli', 'followers': 1033724, 'name': 'Guapdad 4000', 'followers': 975759}
 
