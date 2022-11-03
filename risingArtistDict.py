4d1ec41f4962793cd0c3c188b78e35b79bc5e087
Master-Leo <emillio3198@gmail.com>
2022-11-03 11:40:32 -0700

    'updated mainstream api'

---
+++ risingArtistDict.py
@@ -0,0 +1,8 @@
+risingArtists = ['Joji': 30050750, 'NIKI': 10007653, 'Rich Brian': 8996136,'BIBI': 3588317,'Jackson Wang': 2677250
+'CHUNG HA':
+2667708,
+'Stephanie Poetri':
+1620564,
+'Higher Brothers':
+1577571]
+
