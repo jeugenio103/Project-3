6f79dd57fe7ca297e41254868e523d4a56640f86
Master-Leo <emillio3198@gmail.com>
2022-11-10 19:56:17 -0800

    'updated 88rising folder'

--- risingArtistDict.py
+++
@@ -1,3 +0,0 @@
-artists = {'name': 'Joji', 'followers': 30050750, 'name': 'NIKI', 'followers': 10007653, 'name': 'Rich Brian', 'followers': 8996136,'name': 'BIBI', 'followers': 3588317, 'name': 'Jackson Wang', 'followers': 2677250,
-'name': 'CHUNG HA', 'followers': 2667708, 'name': 'Stephanie Poetri', 'followers': 1620564, 'name': 'Higher Brothers', 'followers': 1577571, 'name': 'Milli', 'followers': 1033724, 'name': 'Guapdad 4000', 'followers': 975759}
-
